a37b3490501c13916ec5361080a72db581f7c26c
HDwhu <846870654@qq.com>
2016-03-21 21:44:24 +0800

    添加第四次作业

---
+++ chapter1/第四次作业.py
@@ -0,0 +1,22 @@
+from matplotlib.pylab import *
+import numpy as np
+X = []
+t = []
+dt = 1
+X.append(0.)
+t.append(0)
+V=40
+end_time = 100
+for i in range(int(end_time / dt)):
+    tmp = X[i] + V * dt
+    X.append(tmp)
+    t.append(dt * (i+1))
+    print t[-1],X[-1]
+x = t
+y = X
+plt.plot(x, y)
+plt.xlabel('time')
+plt.ylabel('distance')
+plt.title('SPEED')
+plt.legend()
+plt.show()
